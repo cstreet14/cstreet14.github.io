66170494a7152b0f631cfe84ab26b450858ef7a0
Casey Streeter <caseystreeter14@gmail.com>
2017-06-23 16:45:47 -0600

    Changes

---
+++ CIT230/Sandbox/calc.js
@@ -0,0 +1,12 @@
+var number1 = [4, 5, 6];
+var number2 = document.getElementById("number2");
+
+number2.value = 5.5;
+
+function add(num1, num2) {
+    var total = num1 + num2;
+    return total;
+}
+
+console.log(add(2,2);
+console.log(total);
