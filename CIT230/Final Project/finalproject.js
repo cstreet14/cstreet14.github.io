2224f6f5ae343acf0e9ec650c20cb72b5c3bae67
Casey Streeter <caseystreeter14@gmail.com>
2017-07-15 22:53:00 -0600

    Additional Changes to Final Project

--- CIT230/Final Project/finalproject.js
+++ CIT230/Final Project/finalproject.js
@@ -62,7 +62,8 @@ function homepage(){
             homepageElement.appendChild(listItem);
         });
     });
-}
+};
+
 function impact(){
     getJSON("finalproject.json").then(function(data) {
         console.log('test');
@@ -72,14 +73,36 @@ function impact(){
             console.log(val);
             impactElement.innerHTML = '';
             var listItem = document.createElement('li');
-            var name = document.createElement('h2');
-            var quote = document.createElement('p');
-            name.innerHTML = val.name;
-            quote.innerHTML = '"'  + val.quote + '"';
-            listItem.appendChild(name);
-            listItem.appendChild(quote);
-            homepageElement.appendChild(listItem);
+            var concussion = document.createElement('p');
+            var implications = document.createElement('p');
+            var makeup = document.createElement('p');
+            var care = document.createElement('p');
+            concussion.innerHTML = '"'  + val.concussion + '"';
+            implications.innerHTML = '"'  + val.implications + '"';
+            makeup.innerHTML = '"'  + val.makeup + '"';
+            care.innerHTML = '"'  + val.care + '"';
+            listItem.appendChild(concussion);
+            listItem.appendChild(implications);
+            listItem.appendChild(makeup);
+            listItem.appendChild(care);
+            impactElement.appendChild(listItem);
         });
     });
-}
+};
 
+function statistics(){
+    getJSON("finalproject.json").then(function(data) {
+        console.log('test');
+        console.log(data);
+        var statisticsElement = document.getElementById("statistics");
+        data.statistics.forEach(function(val){
+            console.log(val);
+            statisticsElement.innerHTML = '';
+            var listItem = document.createElement('li');
+            var history = document.createElement('p');
+            history.innerHTML = '"'  + val.history + '"';
+            listItem.appendChild(history);
+            statisticsElement.appendChild(listItem);
+        });
+    });
+};
